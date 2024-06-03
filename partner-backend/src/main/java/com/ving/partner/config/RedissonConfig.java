d5293f287e03968d99574e9909a15ec90c54add0
MyVing <2960532192@qq.com>
2024-06-03 16:05:09 +0800

    update baseURL

--- partner-backend/src/main/java/com/ving/partner/config/RedissonConfig.java
+++ partner-backend/src/main/java/com/ving/partner/config/RedissonConfig.java
@@ -23,14 +23,13 @@ public class RedissonConfig {
 
     private String port;
 
-    private String password;
 
     @Bean
     public RedissonClient redissonClient() {
         // 1. 创建配置
         Config config = new Config();
         String redisAddress = String.format("redis://%s:%s", host, port);
-        config.useSingleServer().setAddress(redisAddress).setDatabase(3).setPassword(password);
+        config.useSingleServer().setAddress(redisAddress).setDatabase(0);
 
         // 2. 创建实例
         RedissonClient redisson = Redisson.create(config);
